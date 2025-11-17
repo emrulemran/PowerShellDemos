657544a4ea9dde3b58d7f84bb68ea6d1e4f428d6
MD EMRUL EMRAN <emrulhasan@gmail.com>
2025-11-16 20:29:05 -0500

    test

--- PSLoops.ps1
+++ PSLoops.ps1
@@ -1,20 +1,36 @@
 $name = "cauliflower"
 
-foreach($char in $name.ToCharArray()){
+foreach ($char in $name.ToCharArray()) {
     Write-Host $char
 }
 
 
 # For loop
 
-for($i=0; $i -le 10; $i++){
-Write-Host $i -ForegroundColor Blue
+for ($i = 0; $i -le 10; $i++) {
+    Write-Host $i -ForegroundColor Blue
 }
 
 # Looping through an array elements
 
 $devices = @("PC", "mac", "android", "blackberry", "PSP4")
 
-foreach($device in $devices){
+foreach ($device in $devices) {
     Write-Host $device -ForegroundColor Red
+}
+
+
+# switch statement:
+
+$weekDay = "Tuesday"
+
+switch ($weekDay) {
+    "Monday" { Write-Host "Today is day Monday" }
+    "Tuesday" { Write-Host "Today is Tuesday" }
+    "Wednesday" { Write-Host "Today is Wednesday" }
+    "Thursday" { Write-Host "Today is Thursday" }
+    "Friday" { Write-Host "Today is Friday" }
+    "Saturday" { Write-Host "Today is Saturday" }
+    "Sunday" { Write-Host "Today is Sunday" }
+    Default { "Not a valid Week day" }
 }
